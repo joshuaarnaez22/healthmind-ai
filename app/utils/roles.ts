0487367c0be04d70443878bccba54b68bf832a70
joshuaarnaez22 <joshuaarnaez22@gmail.com>
2025-02-06 03:38:32 +0800

    ...

--- app/utils/roles.ts
+++ app/utils/roles.ts
@@ -2,6 +2,14 @@ import { Roles } from "@/types/global";
 import { auth } from "@clerk/nextjs/server";
 
 export const checkRole = async (role: Roles) => {
-  const { sessionClaims } = await auth();
-  return sessionClaims?.metadata.role === role;
+  try {
+    const { sessionClaims } = await auth();
+    if (!sessionClaims?.metadata.role) {
+      return false;
+    }
+    return sessionClaims?.metadata.role === role;
+  } catch (error) {
+    console.error("Failed to verify role:", error);
+    return false;
+  }
 };
