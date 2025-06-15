41ffee7268a6109dc9f0c97c0967a93643594235
joshuaarnaez22 <joshuaarnaez22@gmail.com>
2025-06-16 06:57:59 +0800

    ...

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,6 @@ import { clsx, type ClassValue } from 'clsx';
 import { format } from 'date-fns';
 import { twMerge } from 'tailwind-merge';
 import DOMPurify from 'dompurify';
-import { CheckIn } from '@prisma/client';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
